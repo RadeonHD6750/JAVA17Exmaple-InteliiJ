4ecd9c6d369c723e67cec10be9087fbb49a9a380
jimin <thesuffering@naver.com>
2024-07-09 15:25:17 +0900

    2024-07-09
    자바 수업 준비

---
+++ src/class_extends/Person.java
@@ -0,0 +1,13 @@
+package class_extends;
+
+public class Person {
+    public String name;
+
+    public Person(String baseName) {
+        this.name = baseName;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,27 @@
+package helloworld;
+
+
+
+/******************************************************
+*
+* 클래스명 | HelloWorld
+* 	
+* 내용       | 개발환경 구축 확인용
+* 
+* 작성일    | 2023.04.06
+* 소속       | 개발1팀
+* 직급       | 전임강사
+* 성명       | 서지민
+****************************************************/
+
+
+
+public class HelloWorld 
+{
+	//테스트용 주석
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
+
+}
